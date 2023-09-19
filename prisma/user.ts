becc3170567772ed6bcb320c52513aa44be3b4c6
Proprogam <k.kartal9595@gmail.com>
2023-09-19 12:20:30 +0300

    request type changed

--- prisma/user.ts
+++ prisma/user.ts
@@ -1,17 +1,29 @@
 import prisma from "./prisma";
 import { hash } from "argon2";
 
-export const getAllUsers = async () => {
-  const users = await prisma.user.findMany({});
-  return users;
-};
+// export const getAllUsers = async () => {
+//   const users = await prisma.user.findMany({});
+//   return users;
+// };
+export async function getAllUsers() {
+  try {
+    const users = await prisma.user.findMany({});
+    return users;
+  } catch (error) {
+    return error;
+  }
+}
 
-export const getUser = async (id: string) => {
-  const user = await prisma.user.findUnique({
-    where: { id },
-  });
-  return user;
-};
+export async function getUser(id: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+    return user;
+  } catch (error) {
+    return error;
+  }
+}
 
 export async function createUser(
   email: string,
@@ -33,31 +45,65 @@ export async function createUser(
   }
 }
 
-export const updateUser = async (
+// export const updateUser = async (
+//   id: string,
+//   email: string,
+//   name: string,
+//   password: string
+// ) => {
+//   const hashedPassword = await hash(password);
+//   const user = await prisma.user.update({
+//     where: {
+//       id,
+//     },
+//     data: {
+//       email,
+//       password: hashedPassword,
+//       name,
+//     },
+//   });
+//   return user;
+// };
+export async function updateUser(
   id: string,
   email: string,
   name: string,
   password: string
-) => {
-  const hashedPassword = await hash(password);
-  const user = await prisma.user.update({
-    where: {
-      id,
-    },
-    data: {
-      email,
-      password: hashedPassword,
-      name,
-    },
-  });
-  return user;
-};
-
-export const deleteUser = async (id: string) => {
-  const user = await prisma.user.delete({
-    where: {
-      id,
-    },
-  });
-  return user;
-};
+) {
+  try {
+    const hashedPassword = await hash(password);
+    const user = await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+      },
+    });
+    return user;
+  } catch (error) {
+    return error;
+  }
+}
+// export const deleteUser = async (id: string) => {
+//   const user = await prisma.user.delete({
+//     where: {
+//       id,
+//     },
+//   });
+//   return user;
+// };
+export async function deleteUser(id: string) {
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+    return user;
+  } catch (error) {
+    return error;
+  }
+}
